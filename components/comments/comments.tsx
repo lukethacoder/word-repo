6142c2c8f977cf811d5cd32617b95ce92f0edde1
Luke Secomb <luke.secomb@live.com>
2023-01-13 13:52:56 +0100

    🐛 fix gicus after initial site load not working

--- components/comments/comments.tsx
+++ components/comments/comments.tsx
@@ -1,27 +1,20 @@
-import Script from 'next/script'
+import Giscus from '@giscus/react'
 
 export const Comments = () => {
   return (
-    <>
-      <Script
-        strategy='afterInteractive'
-        src={`https://giscus.app/client.js`}
-        data-repo={process.env.NEXT_PUBLIC_GITHUB_REPO as `${string}/${string}`}
-        data-repo-id={process.env.NEXT_PUBLIC_GITHUB_REPO_ID as string}
-        data-category={process.env.NEXT_PUBLIC_GITHUB_CATEGORY as string}
-        data-category-id={process.env.NEXT_PUBLIC_GITHUB_CATEGORY_ID as string}
-        data-mapping='title'
-        data-strict='0'
-        data-reactions-enabled='1'
-        data-emit-metadata='0'
-        data-input-position='bottom'
-        data-theme='transparent_dark'
-        data-lang='en'
-        data-loading='lazy'
-        crossOrigin='anonymous'
-        async
-      />
-      <div className='giscus'></div>
-    </>
+    <Giscus
+      id='comments'
+      repo={process.env.NEXT_PUBLIC_GITHUB_REPO as `${string}/${string}`}
+      repoId={process.env.NEXT_PUBLIC_GITHUB_REPO_ID as string}
+      category={process.env.NEXT_PUBLIC_GITHUB_CATEGORY as string}
+      categoryId={process.env.NEXT_PUBLIC_GITHUB_CATEGORY_ID as string}
+      mapping='title'
+      reactionsEnabled='1'
+      emitMetadata='0'
+      inputPosition='top'
+      theme='transparent_dark'
+      lang='en'
+      loading='lazy'
+    />
   )
 }
